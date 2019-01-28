7dfe4b1017f2d880905132fd2109371925562b5e
Sakura Mo <moyinzi@outlook.com>
2019-01-28 14:22:39 +0800

    fix build.sh

--- dssds/build.sh
+++ dssds/build.sh
@@ -6,7 +6,7 @@ for GOOS in darwin linux; do
 done
 
 for GOOS in windows; do
-    for GOARCH in 386 amd64 arm; do
+    for GOARCH in 386 amd64; do
         echo $GOOS-$GOARCH
         CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -v -o ./build/dssds-$GOOS-$GOARCH.exe ./dssds
     done
